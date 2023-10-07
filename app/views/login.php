a5356fbbf4ccb7a72f5cc1034d74be6d14819a3c
Matheus Aguilar Taceo <theusaguilar2@gmail.com>
2023-10-07 19:44:50 -0400

    preparando para o merge

--- app/views/login.php
+++ app/views/login.php
@@ -1,54 +1,14 @@
 
-<style>
-      body{
-          font-family: Arial, Helvetica, sans-serif;
-          background-image: linear-gradient(45deg,violet, blue);
-      }
-      .loginn{
-          background-color: rgba(0, 0, 0, 0.9);
-          position: absolute;
-          top: 50%;
-          left: 50%;
-          transform: translate(-50%, -50%);
-          padding: 60px;
-          height: 350px;
-          border-radius: 15px;
-          color: white;
-      }
-      input{
-          padding: 15px; 
-          border: none;
-          outline: none;
-          font-size: 15px;
-          color: black  ;
-      }
-      .inputSubmit{
-          background-color: dodgerblue;
-          border: none;
-          padding: 15px;
-          width: 100%;
-          border-radius: 10px;
-          color: white;
-          font-size: 15px;
-      }
-      .inputSubmit:hover{
-          background-color: blueviolet;
-          cursor: pointer;
-      }
-  </style>  
-
-  <div class="loginn">
-    <form action="login" method="POST">
-      <h1>Login</h1>
-      <?php echo getFlash('messageLogin')?>
-      <br>
-      <input type="text" name = "email" placeholder="Email">
-      <br><br>
-      <input type="password" name="password"  placeholder= "Senha">
-      <br><br>
-      <input class="inputSubmit" type="submit" name="submit" value="Fazer login">
+<div class="container">
+    <h2 class="container-h2">Login de Usuário</h2>
+    <form action="#" method="POST">
+        <label class="container-label" for="email">E-mail:</label>
+        <input type="email" id="email" name="email" required>
+        
+        <label class="container-label" for="senha">Senha:</label>
+        <input type="password" id="senha" name="senha" required>
+        
+        <button class="container-button" type="submit">Entrar</button>
     </form>
-  </div>
-  
-
+</div>
 
