c14dc803f439060ceba33217be9dae2e73e83b24
Matheus Aguilar Taceo <theusaguilar2@gmail.com>
2023-10-10 20:45:15 -0400

    Alterção no CSS geral e no HTML da Home

--- app/views/header_structure/header.php
+++ app/views/header_structure/header.php
@@ -1,27 +1,30 @@
-<div class="header-logo">
+
+
+<div class="header-logo container-fluid">
     <img src="<?php echo IMGS?>png/logo-novo.png" alt="Imagem">
-    </div>
-        
-    <nav class="header-container">
-        <ul class="header-ul-menu">            
-            <li class="header-icon"><a> <img src="<?php echo IMGS?>png/casa.png" alt=""></a></li>
-            <li class="header-menu"><a href="." id="pag">Home</a></li>
+</div>
 
-            <li class="header-icon"><a> <img src="<?php echo IMGS?>png/novo.png" alt=""></a></li>
-            <li class="header-menu"><a href="#" id="pag">New</a></li>
-            <li class="header-icon"><a> <img src="<?php echo IMGS?>png/grafico-button.png" alt=""></a></li>
-            <li class="header-menu"><a href="#" id="pag">Estatistica</a></li>
+<nav class="header-container">
+    <ul class="header-ul-menu container-fluid">
+        <li class="header-icon"><a> <img src="<?php echo IMGS?>png/casa.png" alt=""></a></li>
+        <li class="header-menu"><a href="." id="pag">Home</a></li>
 
-            <li class="header-icon"><a> <img src="<?php echo IMGS?>png/sobre.png" alt=""></a></li>
-            <li class="header-menu"><a href="#" id="pag">Sobre</a></li>
+        <li class="header-icon"><a> <img src="<?php echo IMGS?>png/novo.png" alt=""></a></li>
+        <li class="header-menu"><a href="#" id="pag">New</a></li>
+        <li class="header-icon"><a> <img src="<?php echo IMGS?>png/grafico-button.png" alt=""></a></li>
+        <li class="header-menu"><a href="#" id="pag">Estatistica</a></li>
 
-                <!--BOTÃO MENU-->
-           
-            <li class="header-cad1"><a class="header-anc1" href="register">cadastre-se</a></li>
-            <li class="header-cad2"><a class="header-anc2" href="login">Entrar</a></li>
-        </ul>    
-    </nav>    
+        <li class="header-icon"><a> <img src="<?php echo IMGS?>png/sobre.png" alt=""></a></li>
+        <li class="header-menu"><a href="#" id="pag">Sobre</a></li>
 
-        <div id="conteudo"></div>
+            <!--BOTÃO MENU-->
 
-   
+        <li class="header-cad1"><a class="header-anc1" href="register">cadastre-se</a></li>
+        <li class="header-cad2"><a class="header-anc2" href="login">Entrar</a></li>
+    </ul>
+</nav>
+
+    <div id="conteudo"></div>
+
+
+
