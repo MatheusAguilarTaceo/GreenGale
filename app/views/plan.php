6550611248e1f633dbf9c2c16b19e1c2b98ce26c
joao_antonio <diversaoanimes2021@gmail.com>
2023-10-24 19:03:24 -0400

    Criado a pagina do usuario, para alterar senha e etc.

--- app/views/plan.php
+++ app/views/plan.php
@@ -1,25 +1,83 @@
+<main>
 
-<section>
-    <p>caixa dos planos</p>
-    <p>caixa dos planos</p>
-    <p>caixa dos planos</p>
-    <p>caixa dos planos</p>
-    <p>caixa dos planos</p>
-    <p>caixa dos planos</p>
-    <p>caixa dos planos</p>
-    <p>caixa dos planos</p>
-    <p>caixa dos planos</p>
-    <p>caixa dos planos</p>
-    <p>caixa dos planos</p>
-    <p>caixa dos planos</p>
-    <p>caixa dos planos</p>
-    <p>caixa dos planos</p>
-    <p>caixa dos planos</p>
-    <p>caixa dos planos</p>
-    <p>caixa dos planos</p>
-    <p>caixa dos planos</p>
-    <p>caixa dos planos</p>
-    <p>caixa dos planos</p>
-    <p>caixa dos planos</p>
-    <p>caixa dos planos</p>
-</section>
+<div class="container_plan">
+    
+    <div class="box-plan">
+        <div class="inf" id="user-info-popup">
+            <div class="img">
+                <img src= "<?php echo IMGS?>png/perfildados.png">
+            </div>
+            <p><strong>Nome:</strong></p>
+            <div class="editar"><img src="<?php echo IMGS?>png/editar.png"></div>
+            <p class=" nome">Joao Antonio </p> 
+            <p><strong>Email:</strong></p>
+            <div class="editar"><img src="<?php echo IMGS?>png/editar.png"></div>
+            <p class="nome">diversaoanimesc23@gmail.com</p>
+            <p><strong>Senha:</strong></p>
+            <div class="editar"><img src="<?php echo IMGS?>png/editar.png"></div>
+            <p class="nome">*****************</p>
+        </div>
+
+        <div class="inf">
+            <div class="img">
+                <img src= "<?php echo IMGS?>png/configdados.png">
+            </div>
+            
+            <p><strong>Plano:</strong></p>
+            <div class="plano"><img src="<?php echo IMGS?>png/um.png"></div>
+            <p class=" basico"><strong>Basico</strong></p> 
+            <div class="container-inf-plan">
+                <div class="container-inf-plan-int">
+
+                    <label for="minhaCaixaDeSelecao" class="checkbox-label">
+                        <input type="checkbox" id="minhaCaixaDeSelecao" name="minhaCaixaDeSelecao" class="checkbox">
+                            Modelo 1
+                    </label>
+                    <label for="minhaCaixaDeSelecao" class="checkbox-label">
+                        <input type="checkbox" id="minhaCaixaDeSelecao" name="minhaCaixaDeSelecao" class="checkbox">
+                            Modelo 1
+                    </label>
+                    <label for="minhaCaixaDeSelecao" class="checkbox-label">
+                        <input type="checkbox" id="minhaCaixaDeSelecao" name="minhaCaixaDeSelecao" class="checkbox">
+                            Modelo 1
+                    </label>
+                    <label for="minhaCaixaDeSelecao" class="checkbox-label">
+                        <input type="checkbox" id="minhaCaixaDeSelecao" name="minhaCaixaDeSelecao" class="checkbox">
+                            Modelo 1
+                    </label>
+                    <label for="minhaCaixaDeSelecao" class="checkbox-label">
+                        <input type="checkbox" id="minhaCaixaDeSelecao" name="minhaCaixaDeSelecao" class="checkbox">
+                            Modelo 1
+                    </label>
+                       
+                       
+                </div>
+
+                <div class="container-inf-plan-int">
+                        
+                    <label for="minhaCaixaDeSelecao" class="checkbox-label">
+                    <input type="checkbox" id="minhaCaixaDeSelecao" name="minhaCaixaDeSelecao" class="checkbox">
+                            Modelo 1
+                    </label>
+                    <label for="minhaCaixaDeSelecao" class="checkbox-label">
+                        <input type="checkbox" id="minhaCaixaDeSelecao" name="minhaCaixaDeSelecao" class="checkbox">
+                            Modelo 1
+                    </label>
+                    <label for="minhaCaixaDeSelecao" class="checkbox-label">
+                        <input type="checkbox" id="minhaCaixaDeSelecao" name="minhaCaixaDeSelecao" class="checkbox">
+                            Modelo 1
+                    </label>
+                    <label for="minhaCaixaDeSelecao" class="checkbox-label">
+                        <input type="checkbox" id="minhaCaixaDeSelecao" name="minhaCaixaDeSelecao" class="checkbox">
+                            Modelo 1
+                    </label>
+                    <label for="minhaCaixaDeSelecao" class="checkbox-label">
+                        <input type="checkbox" id="minhaCaixaDeSelecao" name="minhaCaixaDeSelecao" class="checkbox">
+                            Modelo 1
+                    </label>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>  
+</main>
