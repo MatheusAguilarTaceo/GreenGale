98f14aea32dce976d7b20bd14bc48eb859ac93c0
joao_antonio <diversaoanimes2021@gmail.com>
2023-10-24 19:18:53 -0400

    Trocando os nomes dos arquivos de plano e conta, por erro de excrever o codigo no lugar errado

--- app/views/plan.php
+++ app/views/plan.php
@@ -1,83 +1,41 @@
-<main>
 
-<div class="container_plan">
-    
-    <div class="box-plan">
-        <div class="inf" id="user-info-popup">
-            <div class="img">
-                <img src= "<?php echo IMGS?>png/perfildados.png">
-            </div>
-            <p><strong>Nome:</strong></p>
-            <div class="editar"><img src="<?php echo IMGS?>png/editar.png"></div>
-            <p class=" nome">Joao Antonio </p> 
-            <p><strong>Email:</strong></p>
-            <div class="editar"><img src="<?php echo IMGS?>png/editar.png"></div>
-            <p class="nome">diversaoanimesc23@gmail.com</p>
-            <p><strong>Senha:</strong></p>
-            <div class="editar"><img src="<?php echo IMGS?>png/editar.png"></div>
-            <p class="nome">*****************</p>
-        </div>
 
-        <div class="inf">
-            <div class="img">
-                <img src= "<?php echo IMGS?>png/configdados.png">
-            </div>
-            
-            <p><strong>Plano:</strong></p>
-            <div class="plano"><img src="<?php echo IMGS?>png/um.png"></div>
-            <p class=" basico"><strong>Basico</strong></p> 
-            <div class="container-inf-plan">
-                <div class="container-inf-plan-int">
+<div class= "divAccount">
 
-                    <label for="minhaCaixaDeSelecao" class="checkbox-label">
-                        <input type="checkbox" id="minhaCaixaDeSelecao" name="minhaCaixaDeSelecao" class="checkbox">
-                            Modelo 1
-                    </label>
-                    <label for="minhaCaixaDeSelecao" class="checkbox-label">
-                        <input type="checkbox" id="minhaCaixaDeSelecao" name="minhaCaixaDeSelecao" class="checkbox">
-                            Modelo 1
-                    </label>
-                    <label for="minhaCaixaDeSelecao" class="checkbox-label">
-                        <input type="checkbox" id="minhaCaixaDeSelecao" name="minhaCaixaDeSelecao" class="checkbox">
-                            Modelo 1
-                    </label>
-                    <label for="minhaCaixaDeSelecao" class="checkbox-label">
-                        <input type="checkbox" id="minhaCaixaDeSelecao" name="minhaCaixaDeSelecao" class="checkbox">
-                            Modelo 1
-                    </label>
-                    <label for="minhaCaixaDeSelecao" class="checkbox-label">
-                        <input type="checkbox" id="minhaCaixaDeSelecao" name="minhaCaixaDeSelecao" class="checkbox">
-                            Modelo 1
-                    </label>
-                       
-                       
-                </div>
-
-                <div class="container-inf-plan-int">
-                        
-                    <label for="minhaCaixaDeSelecao" class="checkbox-label">
-                    <input type="checkbox" id="minhaCaixaDeSelecao" name="minhaCaixaDeSelecao" class="checkbox">
-                            Modelo 1
-                    </label>
-                    <label for="minhaCaixaDeSelecao" class="checkbox-label">
-                        <input type="checkbox" id="minhaCaixaDeSelecao" name="minhaCaixaDeSelecao" class="checkbox">
-                            Modelo 1
-                    </label>
-                    <label for="minhaCaixaDeSelecao" class="checkbox-label">
-                        <input type="checkbox" id="minhaCaixaDeSelecao" name="minhaCaixaDeSelecao" class="checkbox">
-                            Modelo 1
-                    </label>
-                    <label for="minhaCaixaDeSelecao" class="checkbox-label">
-                        <input type="checkbox" id="minhaCaixaDeSelecao" name="minhaCaixaDeSelecao" class="checkbox">
-                            Modelo 1
-                    </label>
-                    <label for="minhaCaixaDeSelecao" class="checkbox-label">
-                        <input type="checkbox" id="minhaCaixaDeSelecao" name="minhaCaixaDeSelecao" class="checkbox">
-                            Modelo 1
-                    </label>
-                </div>
-            </div>
-        </div>
+    <div class="tab">
+        <!-- Botões da aba -->
+        <button class="tab-button active" onclick="openTab('perfil')">Perfil</button>
+        <button class="tab-button" onclick="openTab('configuracoes')">Configurações</button>
+        <button class="tab-button" onclick="openTab('mensagens')">Mensagens</button>
     </div>
-</div>  
-</main>
+
+    <!-- Conteúdo da aba -->
+    <div id="perfil" class="tab-content active">
+        <h2>Perfil do Usuário</h2>
+        <!-- Conteúdo do perfil do usuário aqui -->
+        <p class="letras">Nome: <?php echo $_SESSION[LOGGED]->name?></p>
+        <p class = "letras">Email:  <?php echo $_SESSION[LOGGED]->email?></p>
+    </div>
+
+    <div id="configuracoes" class="tab-content">
+        <h2>Configurações</h2>
+        <!-- Conteúdo de configurações aqui -->
+        <p>Configurações do usuário podem ser alteradas aqui.</p>
+    </div>
+
+    <div id="mensagens" class="tab-content">
+        <h2>Mensagens</h2>
+        <!-- Conteúdo de mensagens aqui -->
+        <p>Caixa de entrada de mensagens do usuário.</p>
+    </div>
+
+</div>
+<script>
+    function openTab(tabName){
+        // Esconder todo o conteúdo da aba
+        const tabContents = document.getElementsByClassName('tab-content');
+        for (const content of tabContents) {
+            content.style.display = 'none';
+        }
+    }
+<script>   
