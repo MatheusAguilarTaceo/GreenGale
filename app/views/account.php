25e094fe037b30677f7519cc81f70156da6f9e95
Matheus Aguilar Taceo <theusaguilar2@gmail.com>
2023-11-13 21:58:38 -0400

    responsiviade da pagina de account feita para celular, mas eu vou futuramente refatorar todo o codigo da pagina , esta tudo desorganizado

--- app/views/account.php
+++ app/views/account.php
@@ -17,65 +17,65 @@
             <p class="nome" id="password">*****************</p>
         </div>
 
-        <!-- <div class="inf">
-            <div class="img">
-                <img src= "<?php echo IMGS?>png/configdados.png">
-            </div>
-            
-            <p><strong>Plano:</strong></p>
-            <div class="plano"><img src="<?php echo IMGS?>png/um.png"></div>
-            <p class=" basico"><strong>Basico</strong></p> 
-            <div class="container-inf-plan">
-                <div class="container-inf-plan-int">
+    <!-- <div class="inf">
+        <div class="img">
+            <img src= "<?php echo IMGS?>png/configdados.png">
+        </div>
+        
+        <p><strong>Plano:</strong></p>
+        <div class="plano"><img src="<?php echo IMGS?>png/um.png"></div>
+        <p class=" basico"><strong>Basico</strong></p> 
+        <div class="container-inf-plan">
+            <div class="container-inf-plan-int">
 
-                    <label for="minhaCaixaDeSelecao" class="checkbox-label">
-                        <input type="checkbox" id="minhaCaixaDeSelecao" name="minhaCaixaDeSelecao" class="checkbox">
-                            Modelo 1
-                    </label>
-                    <label for="minhaCaixaDeSelecao" class="checkbox-label">
-                        <input type="checkbox" id="minhaCaixaDeSelecao" name="minhaCaixaDeSelecao" class="checkbox">
-                            Modelo 1
-                    </label>
-                    <label for="minhaCaixaDeSelecao" class="checkbox-label">
-                        <input type="checkbox" id="minhaCaixaDeSelecao" name="minhaCaixaDeSelecao" class="checkbox">
-                            Modelo 1
-                    </label>
-                    <label for="minhaCaixaDeSelecao" class="checkbox-label">
-                        <input type="checkbox" id="minhaCaixaDeSelecao" name="minhaCaixaDeSelecao" class="checkbox">
-                            Modelo 1
-                    </label>
-                    <label for="minhaCaixaDeSelecao" class="checkbox-label">
-                        <input type="checkbox" id="minhaCaixaDeSelecao" name="minhaCaixaDeSelecao" class="checkbox">
-                            Modelo 1
-                    </label>
-                       
-                       
-                </div>
-
-                <div class="container-inf-plan-int">
-                        
-                    <label for="minhaCaixaDeSelecao" class="checkbox-label">
+                <label for="minhaCaixaDeSelecao" class="checkbox-label">
                     <input type="checkbox" id="minhaCaixaDeSelecao" name="minhaCaixaDeSelecao" class="checkbox">
-                            Modelo 1
-                    </label>
-                    <label for="minhaCaixaDeSelecao" class="checkbox-label">
-                        <input type="checkbox" id="minhaCaixaDeSelecao" name="minhaCaixaDeSelecao" class="checkbox">
-                            Modelo 1
-                    </label>
-                    <label for="minhaCaixaDeSelecao" class="checkbox-label">
-                        <input type="checkbox" id="minhaCaixaDeSelecao" name="minhaCaixaDeSelecao" class="checkbox">
-                            Modelo 1
-                    </label>
-                    <label for="minhaCaixaDeSelecao" class="checkbox-label">
-                        <input type="checkbox" id="minhaCaixaDeSelecao" name="minhaCaixaDeSelecao" class="checkbox">
-                            Modelo 1
-                    </label>
-                    <label for="minhaCaixaDeSelecao" class="checkbox-label">
-                        <input type="checkbox" id="minhaCaixaDeSelecao" name="minhaCaixaDeSelecao" class="checkbox">
-                            Modelo 1
-                    </label>
-                </div>
+                        Modelo 1
+                </label>
+                <label for="minhaCaixaDeSelecao" class="checkbox-label">
+                    <input type="checkbox" id="minhaCaixaDeSelecao" name="minhaCaixaDeSelecao" class="checkbox">
+                        Modelo 1
+                </label>
+                <label for="minhaCaixaDeSelecao" class="checkbox-label">
+                    <input type="checkbox" id="minhaCaixaDeSelecao" name="minhaCaixaDeSelecao" class="checkbox">
+                        Modelo 1
+                </label>
+                <label for="minhaCaixaDeSelecao" class="checkbox-label">
+                    <input type="checkbox" id="minhaCaixaDeSelecao" name="minhaCaixaDeSelecao" class="checkbox">
+                        Modelo 1
+                </label>
+                <label for="minhaCaixaDeSelecao" class="checkbox-label">
+                    <input type="checkbox" id="minhaCaixaDeSelecao" name="minhaCaixaDeSelecao" class="checkbox">
+                        Modelo 1
+                </label>
+                    
+                    
             </div>
-        </div> -->
+
+            <div class="container-inf-plan-int">
+                    
+                <label for="minhaCaixaDeSelecao" class="checkbox-label">
+                <input type="checkbox" id="minhaCaixaDeSelecao" name="minhaCaixaDeSelecao" class="checkbox">
+                        Modelo 1
+                </label>
+                <label for="minhaCaixaDeSelecao" class="checkbox-label">
+                    <input type="checkbox" id="minhaCaixaDeSelecao" name="minhaCaixaDeSelecao" class="checkbox">
+                        Modelo 1
+                </label>
+                <label for="minhaCaixaDeSelecao" class="checkbox-label">
+                    <input type="checkbox" id="minhaCaixaDeSelecao" name="minhaCaixaDeSelecao" class="checkbox">
+                        Modelo 1
+                </label>
+                <label for="minhaCaixaDeSelecao" class="checkbox-label">
+                    <input type="checkbox" id="minhaCaixaDeSelecao" name="minhaCaixaDeSelecao" class="checkbox">
+                        Modelo 1
+                </label>
+                <label for="minhaCaixaDeSelecao" class="checkbox-label">
+                    <input type="checkbox" id="minhaCaixaDeSelecao" name="minhaCaixaDeSelecao" class="checkbox">
+                        Modelo 1
+                </label>
+            </div>
+        </div>
+    </div> -->
     </div>
 </div>  
