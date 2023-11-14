f89346aca1468452d2876368c48e1173f2cc203e
Matheus Aguilar Taceo <theusaguilar2@gmail.com>
2023-11-13 21:09:30 -0400

    responsividade da pagina home para o celular foi finalizada

--- app/views/home.php
+++ app/views/home.php
@@ -1,38 +1,38 @@
+
 <div class = "container-fluid">
     <h1 class="main-h1"><?php echo $data['title-page']?></h1>    
-        <ul class="main-aviator">
-            <ul class="box-main-jogo">
-                <a href="#" class="main-aviator-jogo">
-                    <img src= "<?php echo IMGS?>png/aviator oficial.png" alt="Imagem">
-                    <p>
-O jogo 'Aviator' é popular em cassinos online, oferecendo uma experiência rápida e empolgante.</p>
-<p>Contudo, lembre-se dos riscos financeiros e jogue com responsabilidade, compreendendo as regras e probabilidades.</p>
-                </a>
-                <ul class="main-button">
-                    <li  class="box-button">
-                        <img src="<?php echo IMGS?>png/grafico-button.png" alt="">
-                        <a class="text-a" href="aviator">Ver estatistica</a>
-                    </li>
-                </ul>
-            </ul>
-        </ul>
-        <div class="container-resoucer">
-            <span class="box-resource">
-                <img src="<?php echo IMGS?>svg/cloud-arrow-down.svg" alt="">
-                <h3 class="h3">Nenhum download necessário</h3>
-                <p class="text">Para vizualização das informações dos jogos, não há a necessidade de baixar nenhum arquivo adicional, apenas acessar a pagina do seu jogo favorito e pronto, você terá acesso a inúmeros filtros relacionados ao jogo, os quais podem ser usados a seu favor. </p>
-            </span>
-            <span class="box-resource">
-                <img src="<?php echo IMGS?>svg/graph-up.svg" alt="">
-                <h3 class="h3">Dados precisos</h3>
-                <p class="text">Rastreador em tempo real totalmente automaziado dos dados das casas de aposta que contenham os jogos no estilo crash, Aviator, SpaceMan, etc.<br><br>Compare o mesmo jogo em várias casas de apostas para determinar o momento ideal para jogar em cada uma delas.</p>
-            </span>
-            <span class="box-resource">
-                <img src="<?php echo IMGS?>svg/bar-chart.svg" alt="">
-                <h3 class="h3">Estilo de jogos Crash</h3>
-                <p class="text">Os Crash Games são jogos dinâmicos e muito simples. Diferentemente de outros jogos de cassino, para jogar Crash Game não é necessário ter habilidades — basta intuição e um pouco de informação.
-                <br><br>Uma das opções mais conhecidas entre os jogos do tipo Crash Game é o Aviator, também chamado de “jogo do aviãozinho”. O objetivo do jogo Crash Game é obter o maior multiplicador possível e retirar a aposta antes do jogo “crashar”, como dizem os adeptos.
-                </p>
-            </span>
+    <div class="main-aviator">
+        <div class="box-main-jogo">
+            <a href="#" class="main-aviator-jogo text-a">
+                <img src= "<?php echo IMGS?>png/aviator oficial.png" alt="Imagem">
+                <p>O jogo 'Aviator' é popular em cassinos online, oferecendo uma experiência rápida e empolgante.</p>
+                <p>Contudo, lembre-se dos riscos financeiros e jogue com responsabilidade, compreendendo as regras e probabilidades.</p>
+            </a>
+            <div class="main-button">
+                <button class="box-button">
+                    <img src="<?php echo IMGS?>png/grafico-button.png" alt="">
+                    <a class="text-a" href="aviator">Ver estatistica</a>
+                </button>
+            </div>
         </div>
+    </div>
+    <div class="container-resoucer">
+        <span class="box-resource">
+            <img src="<?php echo IMGS?>svg/cloud-arrow-down.svg" alt="">
+            <h3 class="h3">Nenhum download necessário</h3>
+            <p class="text">Para vizualização das informações dos jogos, não há a necessidade de baixar nenhum arquivo adicional, apenas acessar a pagina do seu jogo favorito e pronto, você terá acesso a inúmeros filtros relacionados ao jogo, os quais podem ser usados a seu favor. </p>
+        </span>
+        <span class="box-resource">
+            <img src="<?php echo IMGS?>svg/graph-up.svg" alt="">
+            <h3 class="h3">Dados precisos</h3>
+            <p class="text">Rastreador em tempo real totalmente automaziado dos dados das casas de aposta que contenham os jogos no estilo crash, Aviator, SpaceMan, etc.<br><br>Compare o mesmo jogo em várias casas de apostas para determinar o momento ideal para jogar em cada uma delas.</p>
+        </span>
+        <span class="box-resource">
+            <img src="<?php echo IMGS?>svg/bar-chart.svg" alt="">
+            <h3 class="h3">Estilo de jogos Crash</h3>
+            <p class="text">Os Crash Games são jogos dinâmicos e muito simples. Diferentemente de outros jogos de cassino, para jogar Crash Game não é necessário ter habilidades — basta intuição e um pouco de informação.
+            <br><br>Uma das opções mais conhecidas entre os jogos do tipo Crash Game é o Aviator, também chamado de “jogo do aviãozinho”. O objetivo do jogo Crash Game é obter o maior multiplicador possível e retirar a aposta antes do jogo “crashar”, como dizem os adeptos.
+            </p>
+        </span>
+    </div>
 </div>
