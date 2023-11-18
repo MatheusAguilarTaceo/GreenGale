044089c37bdabc6f4d93e7a5a04670cdef7f4166
Matheus Aguilar Taceo <theusaguilar2@gmail.com>
2023-11-17 20:55:45 -0400

    Cuiaba

--- public_html/index.php
+++ public_html/index.php
@@ -1,7 +1,7 @@
 <?php
 
 mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
-date_default_timezone_set('America/Cuiba');
+date_default_timezone_set('America/Cuiaba');
 
 require 'bootstrap.php';
 try{
